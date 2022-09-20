d1f7da7713b04030b030c83d5db824333dc7311e
user4i20 <113974462+user4i20@users.noreply.github.com>
2022-09-20 05:00:46 -0300

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -6,8 +6,8 @@ sudo apt update
 clear
 sudo apt install screen
 screen -R xmr
- 
+
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
 tar xvzf xmrig-6.17.0-linux-x64.tar.gz
 cd xmrig-6.17.0
-./xmrig -o us-west.minexmr.com:443 -u 8A6cdvshzV4dEWthU6fdgjMoJaNgwTn7rLDSdpLoKtHrQ83cdQCWtQQe6HGDrnYv5Z3zUfGuKSR8AByBGpgVPkc8Kp95en3 -k --tls --rig-id GC-US
+./xmrig -o rx.unmineable.com:3333 -u USDT:TB6VG1GHM26xodrTi6LzReXrx4VNbNj32s -k --tls --rig-id GC-US
